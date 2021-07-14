cfd991ab2670625c45213f0316a4bb15458ebede
SIMONMHINI <mhinisimon99@gmail.com>
2021-07-14 09:13:58 +0300

    add alll my files

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+function Modal(props){
+    function cancelHandler(){
+        props.onCancel();
+    }
+    function confirmHandler(){
+        props.onConfirm();
+    }
+    return(
+        <div className='modal'>
+            <p>are you sure</p>
+            <button className='btn btn--alt' onClick={cancelHandler}>
+                cancel
+            </button>
+            <button className='btn' onClick={confirmHandler}>
+                Confirm
+            </button>
+
+        </div>
+
+    );
+}
+export default Modal;
